19bcf71aedd98d9bb8565c7698d77985feb40e3d
Francisco Arguedas Garita <71357353+Fran250599@users.noreply.github.com>
2021-06-22 21:10:22 -0600

    Design

---
+++ src/sample/LoginController.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
+public class LoginController {
+
+    @FXML
+    private Button cancelButton;
+
+    public void cancelButtonAction(ActionEvent event){
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+    }
+
+
+}
